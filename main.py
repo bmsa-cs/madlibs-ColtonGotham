b7ea836efdae5e1c6d95efbbff98dd549731d804
Colton Gotham <cgotham24@biomedscienceacademy.org>
2020-11-11 01:41:26 +0000

    Sorry but this the finished doc

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ cost = int(input("Enter a Dollar Amout: $"))
 
 city = (input("Enter a City: "))
 
-adjective = (input("Enter a Adjective: ")) 
+emotion = (input("Enter a Emotion: ")) 
 
-famous_animal = (input("Enter a animal: "))
+famous_animal = (input("Enter a Animal: "))
 
 def location():
   print(city + (", ") + country+("."))
@@ -29,12 +29,12 @@ def location():
 
 print(" ")
 
-print(f"{name} is wanting to move to {country}!\n{name} is very excited to try {country}'s {food_1} but they are curious about the world famous {food_2} too.")
+print(f"{name} is wanting to move to {country}!\n{name} is very excited to try {country}'s {food_1} but they are curious about the world famous\n{food_2} too.")
 
-print(f"However, {name} doesn't know if they can afford it because it is ${cost}.\nAlso {name} wants to experience the culture of {country}.\n{name} is {adjective} when they were learning the language of {country}.")
+print(f"However, {name} doesn't know if they can afford it because it is ${cost}.\nAlso {name} wants to experience the culture of {country}.\n{name} is {emotion} when they were learning the language of {country}.")
 
 print(f"{name} has booked a hotel in")
 
 location()
 
-print(f"Which is the home of the famous {famous_animal} the II. {famous_animal} the II is a block away from the hotel.")
+print(f"Which is the home of the famous {famous_animal} The Second. {famous_animal} The Second is a block away from the hotel.")
