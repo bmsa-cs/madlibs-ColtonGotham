45543a19ab91262d9f9e786224e9763c5c88885c
Colton Gotham <cgotham24@biomedscienceacademy.org>
2020-11-11 15:26:21 +0000

    OFFICAL FINAL FINISHED COPY

--- main.py
+++ main.py
@@ -25,10 +25,11 @@ famous_animal = (input("Enter a Animal: "))
 def location():
   print(city + (", ") + country+("."))
 
- 
+transportation = (input("Enter a Way of Transportation: "))
+
 
 print(" ")
-
+ 
 print(f"{name} is wanting to move to {country}!\n{name} is very excited to try {country}'s {food_1} but they are curious about the world famous {food_2} too.")
 
 print(f"However, {name} doesn't know if they can afford it because it is ${cost}.\nAlso {name} wants to experience the culture of {country}.\n{name} is {emotion} when they were learning the language of {country}.")
@@ -37,4 +38,4 @@ print(f"{name} has booked a hotel in")
 
 location()
 
-print(f"Which is the home of the famous {famous_animal} The Second. {famous_animal} The Second is a block away from the hotel.")
+print(f"Which is the home of the famous {famous_animal} The Second. The {famous_animal} The Second exibit is a block away from the hotel.\n When {name} was finished packing {name} got into a {transportation} and went to {country}. THE END")
